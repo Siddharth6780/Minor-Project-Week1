fea7e42fa6ed726f8ccdc7d87506070022520dd4
Siddharth <siddharth6780@gmail.com>
2022-10-15 10:49:12 +0530

    Modified Detection through video file

--- Code/Detection_Through_Video.py
+++ Code/Detection_Through_Video.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import sys
+import glob
+import os
 
 sys.path.insert(0, './Helper')
 from Helper.Motion_Detection import MotionDetection
@@ -8,6 +10,12 @@ isclosed = 0
 
 def main():
     while True:
+    
+        list_of_files = glob.glob('/home/pi/*.mkv') # * means all if need specific format then *.csv
+        print(list_of_files)
+        latest_file = max(list_of_files, key=os.path.getctime)
+        print(latest_file)
+
 
         # Capturing video
         video = cv2.VideoCapture('../pebrin database/Video-7_2019-08-14.wmv')
